8df0b466ef37c3c3a29435e61f0a35b91affb55d
Daniel Neighman <has.sox@gmail.com>
2015-08-23 14:00:12 -0700

    update guardian

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule PhoenixGuardian.Mixfile do
       {:phoenix_html, "~> 1.0"},
       {:phoenix_live_reload, "~> 0.4", only: :dev},
       {:cowboy, "~> 1.0"},
-      {:guardian, "~> 0.4.0"}
+      {:guardian, "~> 0.5.0"},
     ]
   end
 end
