494c23d561fce2b1085f1236f689bfd240ff0136
terakilobyte <terakilobyte@gmail.com>
2016-01-10 18:05:35 -0800

    adds nascent realtime chat feature

--- web/static/js/authorized_socket.js
+++ web/static/js/authorized_socket.js
@@ -1,17 +1,39 @@
-import {Socket} from "deps/phoenix/web/static/js/phoenix"
+import {Socket} from "deps/phoenix/web/static/js/phoenix";
 
-const authSocket = new Socket("/socket")
-authSocket.connect()
+const authSocket = new Socket("/socket");
+authSocket.connect();
 
 
-const token = $('meta[name="guardian_token"]').attr('content')
-const channel = authSocket.channel("authorized:lobby", {guardian_token: token})
-        .join()
-        .receive("ok", resp => { socketTalk(resp.message)})
-        .receive("error", resp => { socketTalk(resp.error) })
+const token = $('meta[name="guardian_token"]').attr('content');
+const channel = authSocket.channel("authorized:lobby", {guardian_token: token});
+channel.join()
+        .receive("ok", resp => {
+          console.log(resp);
+          socketTalk(resp.message)
+        })
+        .receive("error", resp => {
+          console.log(resp);
+          socketTalk(resp.error)
+        });
 
-const socketTalk = (message) => {
-  $('#socket-talk') .text(message);
+channel.on("shout", data => { socketTalk(data.body, data.from); });
+
+const socketTalk = (message, from) => {
+  var out = '<li>'
+  if (from) {
+    out += `<span class='chat-from'>${from}:&nbsp;</span>`
+  }
+  out += message
+  out += '</li>'
+
+  $('#socket-talk').append(out);
 }
 
-export default authSocket
+const sendMessage = (e) => {
+  e.preventDefault();
+  let message = e.target.message.value;
+  e.target.message.value = "";
+  channel.push("shout", {body: message});
+}
+
+export default sendMessage;
