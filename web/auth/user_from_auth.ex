328a46ecc067c037741d4667d2d41705b691ee34
ylankgz <ylankgz@gmail.com>
2016-02-29 13:41:47 +0600

    Refactor auth_and_validate in user_from_auth with 'when in' guard clause

--- web/auth/user_from_auth.ex
+++ web/auth/user_from_auth.ex
@@ -102,29 +102,18 @@ defmodule PhoenixGuardian.UserFromAuth do
     end
   end
 
-  defp auth_and_validate(%{provider: :google} = auth, repo) do
+  defp auth_and_validate(%{provider: service} = auth, repo)  when service in [:google, :facebook] do
     case repo.get_by(Authorization, uid: uid_from_auth(auth), provider: to_string(auth.provider)) do
       nil -> {:error, :not_found}
       authorization ->
         if authorization.uid == uid_from_auth(auth) do
           authorization
         else
-          {:error, :"Google uid mismatch"}
+          {:error, :"#{auth.provider} uid mismatch"}
         end
     end
   end
 
-  defp auth_and_validate(%{provider: :facebook} = auth, repo) do
-    case repo.get_by(Authorization, uid: uid_from_auth(auth), provider: to_string(auth.provider)) do
-      nil -> {:error, :not_found}
-      authorization ->
-        if authorization.uid == uid_from_auth(auth) do
-          authorization
-        else
-          {:error, :"Facebook uid mismatch"}
-        end
-    end
-  end
 
   defp auth_and_validate(auth, repo) do
     case repo.get_by(Authorization, uid: uid_from_auth(auth), provider: to_string(auth.provider)) do
